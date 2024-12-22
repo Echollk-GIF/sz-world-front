928f68b76823e97b929780777a6b2d96ef90d38e
echollk <841290355@qq.com>
2024-12-23 02:14:10 +0800

    feat: mobile navigation

---
+++ src/views/main/components/navigation/mobile/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是移动端的navigation</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/main/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <navigation-vue/>
+</template>
+
+<script setup>
+import navigationVue from './components/navigation/index.vue'
+</script>
+
+<style>
+
+</style>
