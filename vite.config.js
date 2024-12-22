ebbd781715a11d1617a2bcfbccb607c0315f3563
echollk <841290355@qq.com>
2024-12-23 01:40:31 +0800

    feat: use alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { join } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //软链接
+  resolve: {
+    alias: {
+      '@': join(__dirname, '/src')
+    }
+  }
 })
